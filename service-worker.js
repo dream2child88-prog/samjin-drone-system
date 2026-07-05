97710a515014e2260b7d720a0ca4829d7a2361b8
dream2child88-prog <dream2child88@gmail.com>
2026-07-05 23:03:36 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'samjin-pwa-v8-admin-tabs';
+const CACHE_NAME = 'samjin-pwa-v11-emergency-stable';
 const STATIC_ASSETS = ['./','./index.html','./style.css','./app.js','./config.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS).catch(() => undefined))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
